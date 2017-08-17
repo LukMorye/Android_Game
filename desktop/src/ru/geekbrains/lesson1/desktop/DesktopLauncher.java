7f1db8bad5190ea1bbe1beb6fe07c2539db9aa21
Титов Валентин <vtitov@truenorth.pro>
2017-08-17 09:58:27 +0300

    Init commit

---
+++ desktop/src/ru/geekbrains/lesson1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.lesson1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geekbrains.lesson1.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
